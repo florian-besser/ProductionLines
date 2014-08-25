dd2cf2e180525b9a4497498c7cb968e391479cfa
Florian Besser <f.besser@hotmail.com>
2014-08-25 23:16:33 +0200

    Added SimpleScene Rendering example, set up jogl

---
+++ src/main/java/test/SimpleScene.java
@@ -0,0 +1,86 @@
+package test;
+
+import java.awt.Frame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.*;
+import javax.media.opengl.awt.GLCanvas;
+import javax.swing.JFrame;
+
+import com.jogamp.opengl.util.*;
+
+public class SimpleScene implements GLEventListener {
+
+    private double theta = 0;
+    private double s = 0;
+    private double c = 0;
+    
+    private static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+    public static void main(String[] args) {
+        GLProfile glp = GLProfile.getDefault();
+        GLCapabilities caps = new GLCapabilities(glp);
+        GLCanvas canvas = new GLCanvas(caps);
+
+        Frame frame = new JFrame("AWT Window Test");
+        frame.add(canvas);
+        frame.setUndecorated(true);
+		frame.setResizable(false);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        
+        device.setFullScreenWindow(frame);
+
+        canvas.addGLEventListener(new SimpleScene());
+
+        FPSAnimator animator = new FPSAnimator(canvas, 60);
+        //animator.add(canvas);
+        animator.start();
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        update();
+        render(drawable);
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int w, int h) {
+    }
+
+    private void update() {
+        theta += 0.01;
+        s = Math.sin(theta);
+        c = Math.cos(theta);
+    }
+
+    private void render(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+
+        // draw a triangle filling the window
+        gl.glBegin(GL.GL_TRIANGLES);
+        gl.glColor3f(1, 0, 0);
+        gl.glVertex2d(-c, -c);
+        gl.glColor3f(0, 1, 0);
+        gl.glVertex2d(0, c);
+        gl.glColor3f(0, 0, 1);
+        gl.glVertex2d(s, -s);
+        gl.glEnd();
+    }
+}

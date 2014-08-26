db878b95c5871c0fc994ed12d2bcbf9dbffc2616
florian-besser <f.besser@hotmail.com>
2014-08-26 22:26:24 +0200

    Windows again

--- src/main/java/test/SimpleScene.java
+++ src/main/java/test/SimpleScene.java
@@ -82,5 +82,13 @@ public class SimpleScene implements GLEventListener {
 		gl.glColor3f(0, 0, 1);
 		gl.glVertex2d(s, -s);
 		gl.glEnd();
+		gl.glBegin(GL.GL_TRIANGLES);
+		gl.glColor3f(1, 0, 0);
+		gl.glVertex2d(c, c);
+		gl.glColor3f(0, 1, 0);
+		gl.glVertex2d(0, -c);
+		gl.glColor3f(0, 0, 1);
+		gl.glVertex2d(-s, s);
+		gl.glEnd();
 	}
 }
